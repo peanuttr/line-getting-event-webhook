a3c7e983ec5ae5859b94d85a98525c3d3f06617c
chitsanupong <chitsanupong.boonserm@gmail.com>
2022-07-30 23:41:19 +0700

    starting

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const line = require('@line/bot-sdk');
+require('dotenv').config();
+
+const config = {
+  channelAccessToken: process.env.channelAccessToken,
+  channelSecret: process.env.channelSecret
+};
+
+const app = express();
+app.post('/webhook', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result));
+});
+
+const client = new line.Client(config);
+function handleEvent(event) {
+  console.log(event);
+  return client.replyMessage(event.replyToken,{
+    type:'text',
+    text:event.message.text
+  })
+}
+
+app.listen(process.env.Port || 3000,()=>{
+    console.log(`listen on port ${process.env.Port || 3000}`);
+});
